a1040d61faade5533a59cdea496109f613ec7cb4
ABHISHEK Mehra <abhishekmehra1245@gmail.com>
2022-03-14 13:49:34 +0530

    fjinal
    final

--- evalution2/src/index.js
+++ evalution2/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
+
+const fixedcontrol = require("./controlers/fixedcontrol");
+const mastercontrol = require("./controlers/mastercontrol");
+const savingcontrol = require("./controlers/savingcontrl");
+
 const app = express();
 
-app.use(express.json)
+app.use(express.json());
 
-
-
+app.use("/fixedacc", fixedcontrol); 
+app.use("/masteracc", mastercontrol);
+app.use("/savingacc", savingcontrol);
 
 module.exports = app;
